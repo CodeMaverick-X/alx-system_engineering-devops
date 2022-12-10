8f78976e249f7c69d0d34542021fe494ee089c17
reinhard <reinhard2057@gmail.com>
2022-12-10 07:58:05 +0000

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,7 +2,7 @@
 file { '/tmp/school':
 
   ensure  => 'present',
-  content => 'I Love Puppet',
+  content => 'I love Puppet',
   group   => 'www-data',
   owner   => 'www-data',
   mode    => '0744'
