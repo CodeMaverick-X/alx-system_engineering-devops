17cba2bb14c6f1afbe059a9c00b255e914fa70b1
reinhard <reinhard2057@gmail.com>
2023-02-09 00:11:29 +0000

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,26 +6,27 @@ using the rest api
 import requests
 from sys import argv
 
-id = argv[1]
-url1 = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(id)
-url2 = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+if __name__ == '__main__':
+    id = argv[1]
+    url1 = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(id)
+    url2 = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
 
-res_todo = requests.get(url1)
-res_user = requests.get(url2)
+    res_todo = requests.get(url1)
+    res_user = requests.get(url2)
 
-todo = res_todo.json()
-user = res_user.json()
+    todo = res_todo.json()
+    user = res_user.json()
 
-done = 0
-total = 0
-for i in todo:
-    total = total + 1
-    if i['completed']:
-        done = done + 1
+    done = 0
+    total = 0
+    for i in todo:
+        total = total + 1
+        if i['completed']:
+            done = done + 1
 
-print('Employee {} is done\
- with tasks({}/{}):'.format(user['name'], done, total))
+    print('Employee {} is done\
+    with tasks({}/{}):'.format(user['name'], done, total))
 
-for j in todo:
-    if j['completed']:
-        print('     {}'.format(j['title']))
+    for j in todo:
+        if j['completed']:
+            print('     {}'.format(j['title']))
