d3776e4d302eb1c8c43466707c0995ec1558b7be
reinhard <reinhard2057@gmail.com>
2023-02-09 00:23:54 +0000

    task 0

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 """Return info of an employees to do list
-using the rest api
+using the rest api in csv format.
 """
 import requests
 from sys import argv
 import csv
 
+if __name__ == '__main__':
+    id = argv[1]
+    url1 = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(id)
+    url2 = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
 
-id = argv[1]
-url1 = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(id)
-url2 = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+    res_todo = requests.get(url1)
+    res_user = requests.get(url2)
 
-res_todo = requests.get(url1)
-res_user = requests.get(url2)
-
-todo = res_todo.json()
-user = res_user.json()
+    todo = res_todo.json()
+    user = res_user.json()
 
 
-with open('{}.csv'.format(id), 'w') as f:
-    writer = csv.writer(f)
-    for j in todo:
-        todo_com_l = []
-        todo_com_l.append(user['id'])
-        todo_com_l.append(user['name'])
-        todo_com_l.append(j['completed'])
-        todo_com_l.append(j['title'])
-        writer.writerow(todo_com_l)
+    with open('{}.csv'.format(id), 'w') as f:
+        writer = csv.writer(f)
+        for j in todo:
+            todo_com_l = []
+            todo_com_l.append(user['id'])
+            todo_com_l.append(user['name'])
+            todo_com_l.append(j['completed'])
+            todo_com_l.append(j['title'])
+            writer.writerow(todo_com_l)
