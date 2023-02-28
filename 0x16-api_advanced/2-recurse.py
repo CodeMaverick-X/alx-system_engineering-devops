0ce67159e8ea6044991e4e33d46f582da6f3d1b0
reinhard <reinhard2057@gmail.com>
2023-02-28 10:40:20 +0000

    task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,31 +3,32 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], count=0):
+def recurse(subreddit, hot_list=[], anchor=""):
     """returns a list containing the titles of all hot articles
     for a given subreddit"""
-    count = count + 1
 
     api = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'by my helper'}
     params = {'limit': 1}
-    if len(hot_list) > 0:
-        after = list(hot_list[-1].keys())[0]
-        params['after'] = after
+    if anchor != "":
+        params['after'] = anchor
 
-    response = requests.get(api, headers=headers, allow_redirects=False,
-                            params=params)
+    res = requests.get(api, headers=headers, allow_redirects=False,
+                       params=params)
 
-    if response.status_code == 200:
-        data_wrapper = response.json()
-        record = data_wrapper.get('data')
-        title = record.get('children')[0].get('data').get('title')
-        next = record.get('after')
-        if next:
-            hot_list.append({next: title})
-            recurse(subreddit, hot_list, count)
-        else:
-            hot_list = [title for title in list(hot_list.values())]
-            return hot_list
-    else:
+    if res.status_code != 200:
         return None
+
+    item = res.json().get('data').get('children')[0]
+    title = item.get('data').get('title')
+    hot_list.append(title)
+
+    # anchor = item['kind'] + '_' + item['data']['id']
+    anchor = res.json().get('data').get('after')
+
+    # print(anchor)
+    # print(hot_list)
+    if anchor:
+        recurse(subreddit, hot_list, anchor)
+    else:
+        return hot_list
