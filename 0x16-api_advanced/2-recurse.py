7e53e2ca8c4462121a093407aeed0e2a9e1e133d
reinhard <reinhard2057@gmail.com>
2023-02-28 20:57:28 +0000

    task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,34 +3,30 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], anchor=""):
+def recurse(subreddit, hot_list=[]):
     """returns a list containing the titles of all hot articles
     for a given subreddit"""
 
     api = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'by my helper'}
+    headers = {'User-Agent': 'Alx-Project by jude'}
     params = {'limit': 1}
-    if anchor != "":
-        params['after'] = anchor
+    if len(hot_list) > 0:
+        after = list(hot_list[-1].keys())[0]
+        params['after'] = after
 
-    res = requests.get(api, headers=headers, allow_redirects=False,
-                       params=params)
+    response = requests.get(api, headers=headers, allow_redirects=False,
+                            params=params)
 
-    if res.status_code != 200:
-        return None
-
-    item = res.json().get('data').get('children')[0]
-    title = item.get('data').get('title')
-    hot_list.append(title)
-
-    # anchor = item['kind'] + '_' + item['data']['id']
-    anchor = res.json().get('data').get('after')
-
-    # print(anchor)
-    # print(hot_list)
-    if anchor:
-        hot_list = recurse(subreddit, hot_list, anchor)
-    if hot_list:
-        return hot_list
+    if response.status_code == 200:
+        data_wrapper = response.json()
+        record = data_wrapper.get('data')
+        title = record.get('children')[0].get('data').get('title')
+        next = record.get('after')
+        if next:
+            hot_list.append({next: title})
+            recurse(subreddit, hot_list)
+        else:
+            hot_list = [title for title in list(hot_list.values())]
+            return hot_list
     else:
-        None
+        return None
---
+++ 0x16-api_advanced/i_dont_know_why_you_wont_work.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Recurse it!"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], anchor=""):
+    """returns a list containing the titles of all hot articles
+    for a given subreddit"""
+
+    api = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'by my helper'}
+    params = {'limit': 1}
+    if anchor != "":
+        params['after'] = anchor
+
+    res = requests.get(api, headers=headers, allow_redirects=False,
+                       params=params)
+
+    if res.status_code != 200:
+        return None
+
+    item = res.json().get('data').get('children')[0]
+    title = item.get('data').get('title')
+    hot_list.append(title)
+
+    # anchor = item['kind'] + '_' + item['data']['id']
+    anchor = res.json().get('data').get('after')
+
+    # print(anchor)
+    # print(hot_list)
+    if anchor:
+        hot_list = recurse(subreddit, hot_list, anchor)
+    if hot_list:
+        return hot_list
+    else:
+        None
