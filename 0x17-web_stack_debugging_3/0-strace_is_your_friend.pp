4c023d4953cd56e082b7e594b1b8fb0c33a6d5ba
reinhard <reinhard2057@gmail.com>
2023-03-01 11:04:58 +0000

    task 2

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Fixes a wordpess site on lamp stack with php
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+# web_satck debugging 3
