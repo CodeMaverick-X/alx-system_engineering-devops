f9571eb39517d8a3aeef750be7a5cd02387beefc
reinhard <reinhard2057@gmail.com>
2023-03-09 13:50:17 +0000

    task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,5 +2,5 @@
 
 exec { 'fix nginx':
   command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
-  path    => ['/bin', '/usr/bin', '/usr/sbin']
+  path    => '/bin:/usr/bin:/usr/sbin'
 }
